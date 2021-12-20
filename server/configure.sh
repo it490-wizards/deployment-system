69e770ca67eea9b0a47a65cc9af7dbc96a64d393
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-12-20 08:25:37 -0500

    Automatically create MySQL user+database

--- server/configure.sh
+++ server/configure.sh
@@ -5,24 +5,41 @@ if [ $(id -u) -ne 0 ]; then
     exit 1
 fi
 
+# constants
+database=deployment
+virtual_host=deployment
+
 # prompt for new username and password
 read -p "New username: " username
 stty -echo
 read -p "New password: " password
 stty echo
-virtual_host=deployment
 
-# create user and virtual host
+# create RabbitMQ user and virtual host
 rabbitmqctl add_user $username $password
 rabbitmqctl add_vhost $virtual_host
 rabbitmqctl set_permissions --vhost $virtual_host $username '.*' '.*' '.*'
 
+# create MySQL user and database
+mysql << EOF
+CREATE DATABASE IF NOT EXISTS $database;
+CREATE USER IF NOT EXISTS '$username'@'localhost' IDENTIFIED BY '$password';
+GRANT ALL PRIVILEGES ON $database.* TO '$username'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
 # create initialization file
-cat > rabbitmq.ini << EOF
+cat > config.ini << EOF
 [rabbitmq]
 host = "localhost"
 port = 5672
 user = "$username"
 password = "$password"
 vhost = "$virtual_host"
+
+[mysql]
+hostname = "localhost"
+username = "$username"
+password = "$password"
+database = "$database"
 EOF
